44c40212098ec3696adc814ba09c79631f37f4e5
Cade Embery <cadeembery@Cades-MacBook-Pro-2.local>
2019-05-02 13:07:57 +1000

    Form Render Components

--- src/router/routes/Home.vue
+++ src/router/routes/Home.vue
@@ -1,17 +1,19 @@
 <template>
     <wrapper>
         <constrain sm>
-            <h1 v-if="!user">Why, hello there!</h1>
-            <h1 v-if="user">Hey {{user.firstName}}!</h1>
-            <p>
-                If you can see this then you are successfully running the Fluro Vue Boilerplate!
-            </p>
-            <p>
-                A few key details you might want to know
-                <ol>
-                    <li>
-                        Make sure you checkout the <a target="_blank" href="https://fluro-developers.github.io/fluro-vue/#/">Fluro Vue API documentation</a>
-                    </li>
+            <div title>
+                <h1 v-if="!user">Why, hello there!</h1>
+                <h1 v-if="user">Hey {{user.firstName}}!</h1>
+                <p>
+                    If you can see this then you are successfully running the Fluro Vue Boilerplate!
+                </p>
+            </div>
+            <wrapper >
+                <h2 title>
+                    Next Steps
+                </h2>
+                <ol class="lg">
+                    
                     <li>
                         <strong>Create your application in Fluro</strong><br />
                         Create your application in the <a href="https://admin.fluro.io/application" target="_blank">Fluro admin panel</a> and configure it's settings.<br />
@@ -20,25 +22,48 @@
                         <strong>Allow Origin</strong><br />
                         Make sure to add 'http://localhost:8080' as an Allowed Origin in your Application Authentication settings so that you can authenticate while you're developing
                     </li>
-                    If you want to use login/authentication then make sure to add <strong>http://localhost:8080</strong> to your application's while you are developing
+                    <li>
+                        <strong>Update your Development Environment</strong><br />
+                        Edit and update the VUE_APP_REMOTE_URL in the <strong>vue.config.js</strong> file variable to match the domain of your application in Fluro.
                     </li>
                     <li>
-                        and update the VUE_APP_REMOTE_URL in the <strong>vue.config.js</strong> file variable to match the domain you want to sign in on
+                        Make sure you checkout the <a target="_blank" href="https://fluro-developers.github.io/fluro-vue/#/">Fluro Vue documentation</a>
                     </li>
+                </ol>
+            </wrapper>
+            <wrapper sm>
+                <h3 title>
+                    Other goodies and options
+                </h3>
+                <ul>
                     <li>
                         <strong>Font Awesome Icons</strong><br />
                         If you want to use the FontAwesome Pro Library update the '.npmrc' file with your license number
                     </li>
-                </ol>
-            </p>
+                </ul>
+            </wrapper>
             <wrapper sm>
                 <h4>Here's an example of how to render an image</h4>
-                <!-- <fluro-image contain item="5bd6340d1a289a5dfac59369" :width="1920" :height="1280" :spinner="true" /> -->
+                <fluro-image contain item="5bd6340d1a289a5dfac59369" :width="1920" :height="1280" :spinner="true" />
             </wrapper>
+
+
+           
+            <!-- <fluro-code-editor v-model="text" @input="trace" lang="js" :height="200"></fluro-code-editor> -->
+            <!-- <fluro-editor v-model="text" @input="trace" :height="200"></fluro-editor> -->
+
+         <!-- <pre>{{text}}</pre> -->
+
             <div v-for="type in types">
-                <h2>{{type.title}}</h2>
-                <div v-for="definition in type.definitions">
-                    <fluro-content-form v-model="model" :fields="definition.fields"></fluro-content-form>
+                
+                <div v-for="definition in type.definitions" v-if="definition._id != '5cca06b411399d3a111d7f19'">
+
+                    <h2>{{definition.title}}</h2>
+                    <!-- <pre>TOP MODEL IS {{model}}</pre> -->
+
+                   
+
+                    <fluro-content-form @input="update" v-model="model" :fields="definition.fields"></fluro-content-form>
                 </div>
             </div>
         </constrain>
@@ -52,7 +77,7 @@ import SEOMixin from '@/mixins/SEOMixin';
 import UserMixin from '@/mixins/UserMixin';
 
 //Get our components from FluroVue
-import { Layout, FluroContentForm, FluroCodeEditor } from 'fluro-vue';
+import { Layout, FluroContentForm, FluroCodeEditor, FluroEditor } from 'fluro-vue';
 
 /////////////////////////////////////////
 
@@ -61,8 +86,17 @@ export default {
     components: {
         FluroContentForm,
         FluroCodeEditor,
+        FluroEditor,
     },
     methods: {
+        trace(output) {
+            // console.log('Trace:', output);
+            this.$forceUpdate();
+        },
+        update(val) {
+            // console.log('Top level', val);
+            this.$forceUpdate();
+        },
         codeEditorInit(editor) {
             // editor.setReadOnly(true);
         }
@@ -74,9 +108,8 @@ export default {
                 image: '<fluro-image contain item="5bd6340d1a289a5dfac59369" :width="1920" :height="1280" :spinner="true" />',
             },
             realms: [],
-            model: {
-
-            },
+            model: {},
+            text:'',
         }
     },
     asyncComputed: {
@@ -87,7 +120,7 @@ export default {
                 return new Promise(function(resolve, reject) {
 
                     self.$fluro.api.post('/defined', {
-                            types: ['article']
+                            types: ['article'],
                         })
                         .then(function(res) {
 
